2753d9d37862658e8d245dfd87a8f0703b18dced
Oksana <ksushatarasenko@gmail.com>
2026-03-20 09:23:27 +0100

    end polska lekcja 33

--- js/imageZum.js
+++ js/imageZum.js
@@ -2,24 +2,33 @@ document.addEventListener("click", function(e) {
 
     const img = e.target.closest(".zum");
 
-    // если клик по картинке
+    // открыть
     if (img) {
-
         const overlay = document.createElement("div");
         overlay.classList.add("image-overlay");
 
         const bigImg = document.createElement("img");
         bigImg.src = img.src;
 
+        const closeBtn = document.createElement("span");
+        closeBtn.classList.add("image-close");
+        closeBtn.innerHTML = "&times;";
+
+        overlay.appendChild(closeBtn);
         overlay.appendChild(bigImg);
         document.body.appendChild(overlay);
 
         return;
     }
 
-    // если клик по затемнению — закрыть
+    // 🔴 если клик по крестику
+    if (e.target.classList.contains("image-close")) {
+        e.target.closest(".image-overlay").remove();
+        return;
+    }
+
+    // 🔴 если клик по фону (НО НЕ по картинке)
     if (e.target.classList.contains("image-overlay")) {
         e.target.remove();
     }
-
 });
